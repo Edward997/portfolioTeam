062f1e7a20717d167cc8f492ba4ea0bdd4520aeb
Edward997 <edward.navarrete7@gmail.com>
2024-05-14 19:35:57 -0600

    Titel page changed

--- src/pages/featuredProjects.tsx
+++ src/pages/featuredProjects.tsx
@@ -17,6 +17,7 @@ import musicPlayer from '../assets/images/schoolProjects/musicPlayer.png';
 import pathFinder from '../assets/images/personalProjects/pathFinderVisualizer.png';
 import sortVisualizer from '../assets/images/personalProjects/sortVisualizer.png';
 import uxuiDesign from '../assets/images/uxUiDesign/uxuiDesing.png';
+import Footer from '../components/footer';
 
 
 
@@ -462,6 +463,7 @@ const FeaturedProjects = () => {
           </Modal>
         </Container>
       </Box>
+      <Footer />
     </ChakraProvider>
   );
 };

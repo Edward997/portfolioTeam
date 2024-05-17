f8d5745d5cb6e83a21c1a5b0aa7e30b4853fc7b4
Edward997 <edward.navarrete7@gmail.com>
2024-05-17 01:35:32 -0600

    Talent team section Added

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading, Text, Button, ChakraProvider, Container, Link, SimpleGrid, IconButton } from '@chakra-ui/react'; // Importa componentes de Chakra UI según sea necesario
+import { Box, Heading, Text, Button, ChakraProvider, Container, Link, SimpleGrid, IconButton, Flex, AvatarGroup, Avatar, AvatarBadge } from '@chakra-ui/react'; // Importa componentes de Chakra UI según sea necesario
 import Banner from '../components/banner';
 import Footer from '../components/footer';
 import { FaGithub, FaYoutube } from "react-icons/fa"
@@ -11,6 +11,7 @@ import societyEvents from '../assets/images/webProjects/societyEvents.png';
 import guionInstruccional from '../assets/images/webProjects/guion_Instruccional.png'; // Ajusta la ruta según la estructura de tu proyecto
 import pdfGenerator from '../assets/images/desktopProjects/pdf_Generator.jpeg';
 
+
 export interface Project {
     id: number;
     title: string;
@@ -20,6 +21,47 @@ export interface Project {
     githubLink: string;
     githubPagesLink: string;
 }
+
+interface TeamMember {
+    name: string;
+    roles: { role: string; color?: string }[];
+    description: string;
+    avatar: string;
+    portfolio:string;
+}
+
+const team: TeamMember[] = [
+    {
+        name: "Luis Eduardo Zaldivar",
+        roles: [
+            { role: "Backend Developer", color: "blue.500" },
+        ],
+        description: "Luis is an engineer focused and dedicated to building backend problem solving code implementing innovative solutions.",
+        avatar: "/jared_avatar.jpg",
+        portfolio:"https://luis-zaldivar.github.io/Mi-portafolio/"
+    },
+    {
+        name: "Isaac Manjarrez",
+        roles: [
+            { role: "Project Manager", color: "blue.500" },
+            { role: "FullStack Developer" },
+        ],
+        description: "Isaac is a Fullstack developer with strong leadership skills to carry out software development projects and assign tasks to the team.",
+        avatar: "/jared_avatar.jpg",
+        portfolio:"https://theicyspark.github.io/portfolio/"
+    },
+    {
+        name: "Edward Navarrete",
+        roles: [
+            { role: "FullStack", color: "blue.500" },
+            { role: "UI/UX Designer" },
+        ],
+
+        description: "Edward is a software development with a focus on web application design and layout with frontend and backend technologies.",
+        avatar: "/jared_avatar.jpg",
+        portfolio:""
+    },
+];
 
 const HomePage: React.FC = () => {
     // Array de proyectos para mostrar en la página de inicio
@@ -44,13 +86,13 @@ const HomePage: React.FC = () => {
         {
             id: 2,
             title: "Instructional Script",
-            description: 
-            <p>
-                Website development for instructional script activity based on didactic 
-                platform <b style={{ color: "green" }}>HTML</b>, <b style={{ color: "green" }}>CSS </b> 
-                and pure <b style={{ color: "green" }}>JavaScript</b> (vanilla) responsively designed for 
-                adaptation to different devices.
-            </p>
+            description:
+                <p>
+                    Website development for instructional script activity based on didactic
+                    platform <b style={{ color: "green" }}>HTML</b>, <b style={{ color: "green" }}>CSS </b>
+                    and pure <b style={{ color: "green" }}>JavaScript</b> (vanilla) responsively designed for
+                    adaptation to different devices.
+                </p>
             ,
             image: guionInstruccional,
             youtubeLink: "",
@@ -60,13 +102,13 @@ const HomePage: React.FC = () => {
         {
             id: 3,
             title: "PDF Report Generator",
-            description: 
-            <p>
-                Desktop application to <b style={{ color: "green" }}>generate reports</b> in pdf or excel, 
-                process automation, made with <b style={{ color: "green" }}>Java</b> and <b style={{ color: "green" }}>data import</b> of text 
-                files or spreadsheets for extraction and processing of data, information, to visualize graphically 
-                with <b style={{ color: "green" }}>interfaces design</b> and|or data export.
-            </p>
+            description:
+                <p>
+                    Desktop application to <b style={{ color: "green" }}>generate reports</b> in pdf or excel,
+                    process automation, made with <b style={{ color: "green" }}>Java</b> and <b style={{ color: "green" }}>data import</b> of text
+                    files or spreadsheets for extraction and processing of data, information, to visualize graphically
+                    with <b style={{ color: "green" }}>interfaces design</b> and|or data export.
+                </p>
             ,
             image: pdfGenerator,
             youtubeLink: "",
@@ -149,7 +191,63 @@ const HomePage: React.FC = () => {
                 <Box id="skills">
                     <Skills />
                 </Box>
-
+                <Box id="team">
+                    
+                        <Container px={{ base: 4, md: 6 }} maxW="container.xl">
+                            <Flex direction="column" alignItems="center" justifyContent="center" textAlign="center">
+                                <Box mb={6}>
+                                    <Text fontSize={{ base: '3xl', sm: '5xl' }} fontWeight="bold">
+                                        Our Talented Team
+                                    </Text>
+                                    <Text maxW="900px" fontSize={{ md: 'xl', lg: 'base', xl: 'xl' }} color="gray.500">
+                                        Meet the individuals who make our success possible.
+                                    </Text>
+                                </Box>
+                            </Flex>
+                            <Flex mx="auto" maxW="5xl" alignItems="center" gap={6} py={12} flexWrap={{ base: 'wrap', lg: 'nowrap' }} justifyContent="center">
+                                {team.map((member, index) => (
+                                    <Flex
+                                        key={index}
+                                        direction="column"
+                                        alignItems="center"
+                                        mx={4}
+                                        w={{ base: '100%', md: 'auto' }}
+                                        maxW={{ base: '100%', md: '200px' }}
+                                    >
+                                        <AvatarGroup>
+                                            <Avatar src={member.avatar} size="xl">
+                                                <AvatarBadge boxSize="1em" bg="green.500" />
+                                            </Avatar>
+                                        </AvatarGroup>
+                                        <Flex direction="column" alignItems="center" mt={4}>
+                                            <Text fontSize="lg" fontWeight="bold" textAlign="center">{member.name}</Text>
+                                            <Flex direction="column" alignItems="center" textAlign="center">
+                                                {member.roles.map((role, idx) => (
+                                                    <Text key={idx} fontSize="sm" color={role.color || 'gray.500'}>
+                                                        {role.role}
+                                                    </Text>
+                                                ))}
+                                            </Flex>
+                                            <Text fontSize="sm" color="gray.500" mt={2} textAlign="center">{member.description}</Text>
+                                            <Button
+                                                as="a"
+                                                href={member.portfolio}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                mt={4}
+                                                colorScheme="teal"
+                                                size="sm"
+                                                
+                                            >
+                                                View Portfolio
+                                            </Button>
+                                        </Flex>
+                                    </Flex>
+                                ))}
+                            </Flex>
+                        </Container>
+                    
+                </Box>
                 <Box id="contact">
                     <Contact />
                 </Box>
